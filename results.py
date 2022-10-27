36b4d90fb1b65e33f1210ec4ebb9e43a568305e3
tldata1 <thomaslux@live.de>
2022-10-27 11:25:23 +0200

    Implemented output_type
    #1 Issue.
    Addressed feature request by tldata1.

--- results.py
+++ results.py
@@ -4,5 +4,5 @@ import TLux_package
 # print(TLux_package.kaya_fct.__doc__)
 
 germany_co2 = TLux_package.kaya_fct(82.4, 44, 5, 0.05)
-print(germany_co2)
-
+germany_carbon = TLux_package.kaya_fct(82.4, 44, 5, 0.05, 'C')
+print(germany_co2, germany_carbon)
