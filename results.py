95ef11493779014e1355e2180f6cdf5be9692e61
tldata1 <90393878+tldata1@users.noreply.github.com>
2022-10-31 10:09:28 +0100

    Slightly modified results.py

--- results.py
+++ results.py
@@ -1,8 +1,9 @@
 import TLux_package
 
 
-# print(TLux_package.kaya_fct.__doc__)
+print(TLux_package.kaya_fct.__doc__)
 
 germany_co2 = TLux_package.kaya_fct(82.4, 44, 5, 0.05)
 germany_carbon = TLux_package.kaya_fct(82.4, 44, 5, 0.05, 'C')
-print(germany_co2, germany_carbon)
+print('CO2 emissions of Germany: ', germany_co2)
+print('Carbon emissions of Germany: ', germany_carbon)
